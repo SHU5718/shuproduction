4bfc544513fdd8b97da1adc81074a5aa2e746a82
欧ヤン 浚林 <rossouyang31@gmail.com>
2021-10-13 14:35:45 +0900

    added database

---
+++ database/migrations/2021_10_13_121426_create_users_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name', 16)->unique();
+            $table->string('user_email', 255)->unique();
+            $table->char('usr_password', 32);
+            $table->string('user_img', 64)->nullable()->comment('ユーザーアイコン');
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_img', 64);
+            $table->datetime('product_time')->comment('投稿日時');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('comment_flag')->default(0);
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('comments');
+    }
+}
