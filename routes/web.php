4606ffd58e5241f62c32cfb417af651d9fc57f55
SHU1230 <shu_20001230@yahoo.co.jp>
2021-10-14 11:19:43 +0900

    add

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('top');
+    return view('welcome');
+});
+
+Route::get('/top', function(){
+    return view('top')
 });
