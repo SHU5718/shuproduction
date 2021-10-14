d433c2ca4cd2e941b0f8c778885f281ad39b5eae
Gakuto416 <yomogisushi@gmail.com>
2021-10-14 12:21:28 +0900

    add

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class PostController extends Controller
 {
-    
+    public function index()
+    {
+
+        $url = "https://api.a3rt.recruit.co.jp/text_suggest/v2/predict?apikey=DZZKDP8fwYaKERZNzljBTPuKgFp5RmE7&previous_description=ウマ娘です";
+        $method = "GET";
+
+        //接続
+        $client = new Client();
+        $response = $client->request($method, $url);
+        $posts[] = json_decode($response->getBody()->getContents(), true);
+        foreach ($posts as $post) {
+          if (isset($post['suggestion'])) {
+            $suggestion = $post['suggestion'];
+          }
+        }
+        return view('index', ['posts' => $suggestion]);
+    }
 }
