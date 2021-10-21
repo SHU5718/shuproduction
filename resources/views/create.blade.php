02cd1bd8ca0f929c349b221bf0e03eb6795f75a2
Gakuto416 <yomogisushi@gmail.com>
2021-10-21 15:10:48 +0900

    add

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.common')
+
 @section('content')
-<h1>新規登録チェック</h1>
-<p>{{$msg}}</p>
+<div class="container">
+  <div class="row mt-4">
+    <div class="box1 col-lg-8">
+      <div class="row">
+        <p>{{$msg}}</p>
+      </div>
+    </div>
+  </div>
+</div>
+
 @endsection
