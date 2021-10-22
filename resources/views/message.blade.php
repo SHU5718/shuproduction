87da223fb6f3b45843cc27392b9bc2fdbc9af716
Gakuto416 <yomogisushi@gmail.com>
2021-10-22 10:41:12 +0900

    add

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row mt-4">
-        <div class="box1 col-lg-8">
-            <div class="row">
-                <p>{{$msg}}</p>
-            </div>
-        </div>
+  <div class="row mt-4">
+    <div class="box1 col-lg-8">
+      <div class="row">
+        <p>{{$msg}}</p>
+      </div>
     </div>
+  </div>
 </div>
 
 @endsection
